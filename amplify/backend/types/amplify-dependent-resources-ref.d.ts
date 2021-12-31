79202b610ff2975e1c4ccc361515d25147ad9858
Gino Swanepoel <ginoleemusic@live.com>
2021-12-31 12:04:57 +0200

    added amplify resources

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "chatbotwithamplify": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "interactions": {
+        "lex4b84bd6e": {
+            "Region": "string",
+            "BotName": "string",
+            "FunctionArn": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify from "aws-amplify";
+import React from "react";
+import "./App.css";
+import awsconfig from "./aws-exports";
+import Chatbot from "./components/Chatbot/Chatbot";
+
+Amplify.configure(awsconfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Chatbot />;
 }
 
 export default App;
